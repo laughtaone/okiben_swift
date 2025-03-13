6a04a7d1c5cc8c191d74eb60e9cfd56fb1ac2cb7
Taichi USUBA <m21018@g.metro-cit.ac.jp>
2025-03-13 16:11:30 +0900

    Initial Commit

---
+++ okiben/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  okiben
+//
+//  Created by 薄葉太一 on 2025/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ okiben/okibenApp.swift
@@ -0,0 +1,17 @@
+//
+//  okibenApp.swift
+//  okiben
+//
+//  Created by 薄葉太一 on 2025/03/13.
+//
+
+import SwiftUI
+
+@main
+struct okibenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
